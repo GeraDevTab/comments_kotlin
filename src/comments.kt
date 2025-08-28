140c4a4cea1b9343289ffae4dfec16c1a5e2e3c0
Gerardo <everythingissofast@gmail.com>
2025-08-27 22:08:26 -0600

    first commit comments kotlin

---
+++ src/comments.kt
@@ -0,0 +1,8 @@
+fun main(){
+    //the line below will be ignored
+    //println("Hello, World")
+
+    //This prints the string "Hello, Kotlin"
+    println("Hello, Kotlin") //here can be any comment
+    /// Valid single-line comment
+}
