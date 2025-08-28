030c18fa1feb310c1048eb7164126d9af073efda
Gerardo <everythingissofast@gmail.com>
2025-08-27 23:04:49 -0600

    add docs comments

--- src/comments.kt
+++ src/comments.kt
@@ -5,4 +5,43 @@ fun main(){
     //This prints the string "Hello, Kotlin"
     println("Hello, Kotlin") //here can be any comment
     /// Valid single-line comment
+
+    //MULTI-LINE COMMENTS
+
+    /* This is a single-line comment */
+    /* This is an example of
+    a multi-line comment
+     */
+
+    /*** Valid multiline comment
+     println("Hello")
+     println("World")
+     **/
+
+    /*
+    System.out.println("Hello") // print "Hello"
+    System.out.println("Kotlin") /* print "Kotlin" */
+     */
+
+    //DOCUMENTATION COMMENTS (DOC COMMENTS)
+    /**
+     * The 'main' function accepts string arguments from outside
+     * @param args arguments from the command line.
+     * fun main(args: Array){
+     * //do something
+     * }
+     */
+
+    //example of documentation comments
+    /**
+     * calculates the area of a rectangle
+     * @param length The length of the rectangle
+     * @param widtg The width of the rectangle
+     * @return The area of the rectangle
+     * fun calculateRectangleArea(length:Double, width: Double):Double{
+     * return length * width
+     */
+
+
+
 }
